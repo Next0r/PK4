184af9794d0d142614fcfb5becd84f720c580cc1
Next0r <mchkafka@gmail.com>
2018-03-08 15:48:54 +0100

    a

--- Template/Template/Template.cpp
+++ Template/Template/Template.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+//przeciazanie metod dla roznych typow
 class baza {
 private:
 	int size1;
@@ -42,6 +43,8 @@ public:
 	~baza() {};
 };
 
+//klasa realizujaca te same zadaniania zbudowana za pomoca szablonu (brak koniecznosci przeciazania)
+
 template<typename t1>
 class b_dowolna {
 protected:
@@ -65,6 +68,8 @@ public:
 	~b_dowolna() {};
 };
 
+// dziedziczenie po szablonie klasy, szablon z wieksza iloscia typow
+
 template <typename t1, typename t2>
 class b_dowolna2 : protected b_dowolna<t1> {
 private:
@@ -81,7 +86,7 @@ public:
 
 	void wypisz_wszystko() {
 		wypisz();
-		cout << "----" << endl;
+		cout << "-----" << endl;
 		for (int i = 0; i < size2; i++) {
 			cout << tablica2[i] << endl;
 		}
@@ -90,11 +95,14 @@ public:
 	~b_dowolna2() {};
 };
 
+
+// dziedziczenie po szablonie 2
 template <typename t1>
 class b_dowolna_mod : public b_dowolna<t1> {
 public:
 	b_dowolna_mod(t1 tab1[], int s1) : b_dowolna(tab1, s1) {};
 
+	// szablon metody, rezultat zalezny od typu danych
 	template<typename t2>
 	void pobierz(t2 zmienna) {
 		if (typeid(t2) == typeid(t1)) {
@@ -106,6 +114,35 @@ public:
 	}
 };
 
+// kontener do zarzadzania za pomoca funkcji (przyklad wykorzystania wskaznika jako typu)
+class kalkulator {
+public:
+	float liczba1;
+	float liczba2 = 0;
+
+	void mnozenie(int l2) {
+		for (int i = 0; i < l2; i++) {
+			liczba2 += liczba1;
+		}
+	}
+
+	kalkulator(float l1) {
+		liczba1 = l1;
+	}
+
+	~kalkulator() {};
+};
+
+//standardowy szablon funckji
+template <typename wsk>
+void suma_wypisz(wsk* o1, wsk *o2) {
+	if (typeid(wsk) == typeid(kalkulator)) {
+		cout << (o1->liczba2 + o2->liczba2) << endl; // operacja zalezna od przekazywanych wskazników
+	}
+	else {
+		cout << "wprowadzonych danych nie mozna zsumowac" << endl;
+	}
+}
 
 
 
@@ -115,11 +152,15 @@ int main()
 	const int s2 = 3;
 	int tab1[s1] = { 1,2,3,4,5 };
 	float tab2[s2] = { 3.275,1.76,3.98 };
+	kalkulator *k1 = new kalkulator(15.123);
+	kalkulator *k2 = new kalkulator(22.562);
 
+	//wypisywania z bazy z przeciazonymi metodami
 	baza *db1 = new baza(tab1, tab2, s1, s2);
 	db1->wypisz();
 	cout << "-----" << endl;
 
+	//tworzenie dwoch obiektow dla roznych typow danych z szablonu 1
 	b_dowolna<int> *db2 =  new b_dowolna<int>(tab1, s1);
 	b_dowolna<float> *db3 = new b_dowolna<float>(tab2, s2);
 	db2->wypisz();
@@ -127,10 +168,12 @@ int main()
 	db3->wypisz();
 	cout << "-----" << endl;
 	
+	// wykorzystywanie klasy z szablonu dziedziczonego
 	b_dowolna2<int, float> *db4 = new b_dowolna2<int, float>(tab1, tab2, s1, s2);
 	db4->wypisz_wszystko();
 	cout << "-----" << endl;
 
+	// podmienianie jednego z obiektow w tablicy (zalezne od typu)
 	b_dowolna_mod<float> *db5 = new b_dowolna_mod<float>(tab2, s2);
 	db5->pobierz((bool)false); // nie mozna wpisac false do tablicy zmiennoprzecinkowej
 	cout << "-----" << endl;
@@ -138,6 +181,12 @@ int main()
 	db5->wypisz();
 	cout << "-----" << endl;
 
+	k1->mnozenie(5);
+	k2->mnozenie(7);
+	
+	//stosowanie szablonu standardowej funkcji, wskazniki jako typ danych
+	suma_wypisz(k1, k2);
+	
     return 0;
 }
 
